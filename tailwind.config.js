4e12a73e30e96f72fc19d17533b2ba7d78c26dd4
Shakshi Mittal <115569423+Shakshi-Mittal@users.noreply.github.com>
2024-01-03 02:40:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,css}', // Adjust the path based on your project structure
+    // Add other content sources if needed
+  ],
+  darkMode: 'media', // Update from 'false' to 'media'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
